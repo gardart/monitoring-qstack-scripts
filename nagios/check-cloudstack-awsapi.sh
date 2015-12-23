8dc93e1724a99f3296b7236374b5c8db1077850a
ilias <ilias@greenqloud.com>
2015-12-23 13:24:17 +0000

    initial awsapi log check
    Change-Id: I8566aadbbdbea4aa9e4e939cbde00e09615e0471

---
+++ nagios/check-cloudstack-awsapi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pushd `dirname $BASH_SOURCE` > /dev/null; HOME=`pwd`; popd > /dev/null
+source $HOME/common.sh "$1"
+
+file="/var/log/cloudstack/awsapi/awsapi.log"
+current=`date +%s`
+last_modified=`stat -c "%Y" $file`
+if [ $(($current-$last_modified)) -gt 180 ]; then
+	echo "WARNING";
+	exit 1
+else
+	echo "GREEN";
+	exit 0
+fi
